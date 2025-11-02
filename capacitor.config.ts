53012d4e5bcf03757ed9534ba7c3e1a69ece4c47
Diana Rosado <drosado@its.jnj.com>
2025-11-02 16:01:20 -0500

    android support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fabulosita.todochecklist',
+  appName: 'Todo Checklist',
+  webDir: 'dist'
+};
+
+export default config;
